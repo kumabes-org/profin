e934f24f1e8d35785556835606fe918bbe7b264b
Rodrigo Tomio Kumabe <rodrigo.kumabe@gmail.com>
2024-06-23 07:56:25 -0300

    feat: setup basics confs

---
+++ app/src/main/java/com/example/profin/ProfinApplication.java
@@ -0,0 +1,13 @@
+package com.example.profin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfinApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/example/profin/ProfinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
